fe963bf651639e1276f64b4b1035bd7041d37874
Jeroen Smink <jeroensmink98@gmail.com>
2023-06-22 22:01:21 +0200

    build(docker): add support for output path in Dockerfile ENTRYPOINT feat(docker): add ci.buildStatic option to unlighthouse.config.ts feat(docker): add task-completed hook to unlighthouse.config.ts to run a command after a task is completed feat(docker): add worker-finished hook to unlighthouse.config.ts to exit the node process when the worker is finished docs(docker): add README.md to docker-images directory

---
+++ docker-images/README.md
@@ -0,0 +1,4 @@
+# Custom Docker Images
+
+We can build custom docker images of Unlighthouse using the bash script `build-container.sh` in the root of the project.
+
--- docker-images/unlighthouse.config.ts
+++ docker-images/unlighthouse.config.ts
@@ -1,3 +1,5 @@
+import childProcess from 'child_process';
+
 export default {
     puppeteerOptions: {
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
@@ -5,10 +7,26 @@ export default {
     server: {
         open: false,
     },
+    ci: {
+        buildStatic: true
+    },
     cache: false,
     hooks: {
+        // Task completed
+        'task-completed': async (task) => {
+            console.log(`Task completed: ${task.url}`);
+            // Run a command
+            const child = childProcess.spawn('echo', ['Hello, world!'], {
+                detached: true,
+                stdio: 'inherit'  // change this to 'inherit' if you want to see the command output
+            });
+            child.unref();
+        },
+
+        // Exit the node process when the worker is finished
         'worker-finished': async () => {
             process.exit(0);
-        }
+        },
     }
+
 }
